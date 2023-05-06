f762ab47e1d362017f3c45815d544c08b3557af5
jsebasti <jsebasti@student.42barcelona.com>
2023-05-06 20:04:45 +0200

    amb estructura mini

--- INC/minishell.h
+++ INC/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: baltes-g <baltes-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 19:20:45 by baltes-g          #+#    #+#             */
-/*   Updated: 2023/05/06 19:23:22 by baltes-g         ###   ########.fr       */
+/*   Updated: 2023/05/06 20:04:05 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,13 @@ typedef struct  s_token
     t_token *next;
 }       t_token;
 
-
-typedef struct
+typedef struct s_mini
+{
+    t_env   *env;
+    t_token *tok;
+    int     in;
+    int     out;
+    int     pid;
+}   t_mini;
 
 #endif
