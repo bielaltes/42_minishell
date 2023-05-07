d9c85c7332b5625ca6c7db20886aec39f028720f
jsebasti <jsebasti@student.42barcelona.com>
2023-05-07 16:19:32 +0200

    prueba 2

--- SRC/minishell.c
+++ SRC/minishell.c
@@ -6,37 +6,11 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 00:29:36 by jsebasti          #+#    #+#             */
-/*   Updated: 2023/05/07 16:05:34 by jsebasti         ###   ########.fr       */
+/*   Updated: 2023/05/07 16:17:59 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-size_t	array_len(char **arry)
-{
-	size_t	i;
-
-	i = 0;
-	while (arry[i])
-		i++;
-	return (i);
-}
-
-void	init_env(t_mini *mini, char **env)
-{
-	int	i;
-
-	i = 0;
-	mini->env = malloc(sizeof(t_env) * array_len(env));
-	if (!mini->env)
-		return ;
-	while (env[i])
-	{
-		mini->env[i].word = env[i];
-		printf("%s\n", mini->env[i].word);
-		i++;
-	}
-}
 
 int	main(int ac, char **av, char **env)
 {
