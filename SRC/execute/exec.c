835a23a71663753ae70a58e2332f9a9467b35c83
jsebasti <jsebasti@student.42barcelona.com>
2023-05-14 10:15:08 +0200

    si

--- SRC/execute/exec.c
+++ SRC/execute/exec.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: baltes-g <baltes-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 19:01:03 by baltes-g          #+#    #+#             */
-/*   Updated: 2023/05/11 20:14:55 by baltes-g         ###   ########.fr       */
+/*   Updated: 2023/05/14 10:14:27 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    exec(t_mini *mini)
-{
-    int p[2];
-    int i;
+// void    exec(t_mini *mini)
+// {
+//     int p[2];
+//     int i;
 
-    i = 0;
-    while (mini->cmds[i])
-    {
-        if (mini->cmds[i + 1])
-            pipe(p);
-        mini->cmds[i].pid = fork();
+//     i = 0;
+//     while (mini->cmds[i])
+//     {
+//         if (mini->cmds[i + 1])
+//             pipe(p);
+//         mini->cmds[i].pid = fork();
 
-        redir();
-        do_execve();
-        ++i;
-    }    
-}
+//         redir();
+//         do_execve();
+//         ++i;
+//     }    
+// }
