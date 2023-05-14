6f94c34fe0e9d7135380f2943db91b63e94883f2
jsebasti <jsebasti@student.42barcelona.com>
2023-05-14 07:21:06 +0200

    cerca de tener el env

--- SRC/built-ins/env.c
+++ SRC/built-ins/env.c
@@ -6,7 +6,7 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 16:17:21 by jsebasti          #+#    #+#             */
-/*   Updated: 2023/05/12 01:32:37 by jsebasti         ###   ########.fr       */
+/*   Updated: 2023/05/14 07:18:57 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,28 @@ int	exec_env(t_env *env)
 
 void	init_env(t_mini *mini, char **env)
 {
+	t_env	*aux;
 	int		i;
 
 	i = 0;
+	aux = malloc(sizeof(t_env));
+	if (!aux)
+		return ;
+	aux->data = NULL;
+	aux->next = NULL;
+	aux->prev = NULL;
 	while (env[i])
 	{
 		mini->env = malloc(sizeof(t_env));
 		if (!mini->env)
 			return ;
-		mini->env->data = env[i];
-		if (i > 1)
-			mini->env->prev = mini->env;
+		if (i < 1)
+			mini->env->prev = aux;
+		mini->env->data = ft_strdup(env[i]);
+		aux = mini->env;
 		mini->env = mini->env->next;
-		printf("%d\n", i);
+		if (i > 1)
+			mini->env->prev = aux;
 		i++;
 	}
 }
