a3211d5aed3628149c6e2b8d0f42f9be494d84df
jsebasti <jsebasti@student.42barcelona.com>
2023-05-17 23:57:57 +0200

    si

---
+++ OBJ/SRC/built-ins/pwd.d
@@ -0,0 +1,2 @@
+OBJ/SRC/built-ins/pwd.o: SRC/built-ins/pwd.c INC/minishell.h \
+  INC/libft/libft.h
--- SRC/main/minishell.c
+++ SRC/main/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: baltes-g <baltes-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 00:29:36 by jsebasti          #+#    #+#             */
-/*   Updated: 2023/05/17 18:05:23 by baltes-g         ###   ########.fr       */
+/*   Updated: 2023/05/17 20:18:33 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ int main(int argc, char **argv, char**env)
 		mini.cmdline = readline("😇 Macroshell ~ ");
 		add_history(mini.cmdline);
 		lexer(&mini);
-		int i = 0;
-		for (i = 0; mini.tok_lex[i].word != NULL; ++i)
-			printf("%s %d %d\n", mini.tok_lex[i].word, mini.tok_lex[i].expand, mini.tok_lex[i].type);
+		// int i = 0;
+		// for (i = 0; mini.tok_lex[i].word != NULL; ++i)
+		// 	printf("%s %d %d\n", mini.tok_lex[i].word, mini.tok_lex[i].expand, mini.tok_lex[i].type);
 		syntax(&mini);
 		//expand(cmd_line);
 		exec(&mini);
