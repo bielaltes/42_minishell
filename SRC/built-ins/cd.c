3b4513f95bd815055265db4e7a5dd93658075bb1
José Manuel Sebastiá Gómez <jsebasti@jsebasti.local>
2023-05-18 02:00:47 +0200

    mirando un par de casos

--- SRC/built-ins/cd.c
+++ SRC/built-ins/cd.c
@@ -6,24 +6,30 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 17:46:12 by jsebasti          #+#    #+#             */
-/*   Updated: 2023/05/17 23:45:33 by jsebasti         ###   ########.fr       */
+/*   Updated: 2023/05/18 01:08:41 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	search_env(t_env *env, const char *s, int opt)
+int	search_env(t_env *env, const char *s, int opt)
 {
 	if (opt == 1)
 	{
 		while (ft_strncmp(env->data, s, ft_strlen(s)) && env->next)
 			env = env->next;
+		if (ft_strncmp(env->data, s, ft_strlen(s)) && !env->next)
+			return (1);
+		else
+			return (0);
 	}
 	if (opt == 2)
 	{
 		while (env->prev)
 			env = env->prev;
+		return (0);
 	}
+	return (0);
 }
 
 char	*get_env_var(t_env *env, const char *s)
@@ -36,13 +42,14 @@ char	*get_env_var(t_env *env, const char *s)
 	return (var);
 }
 
-static int	update_oldpwd(t_env *env)
+static int	update_oldpwd(t_env *env, t_mini *mini)
 {
 	char	*tmp;
 
 	tmp = NULL;
 	getcwd(tmp, PATH_MAX);
-	search_env(env, "OLDPWD", 1),
+	if (search_env(env, "OLDPWD", 1))
+		exec_export(mini, "OLDPWD");
 	env->data = ft_strjoin("OLDPWD=", tmp);
 	if (!env->data)
 	{
@@ -61,9 +68,10 @@ static int	change_path(t_mini *mini, int options)
 	dir = NULL;
 	if (options == 0)
 	{
-		if (update_oldpwd(mini->env))
+		if (update_oldpwd(mini->env, mini))
 			return (1);
-		search_env(mini->env, "HOME", 1);
+		if (search_env(mini->env, "HOME", 1))
+			exec_export(mini, "HOME");
 		chdir((ft_split(mini->env->data, '='))[1]);
 		search_env(mini->env, "borrar", 2);
 		search_env(mini->env, "PATH", 1);
@@ -79,7 +87,7 @@ static int	change_path(t_mini *mini, int options)
 		if (!dir)
 			return (1);
 		dir = get_env_var(mini->env, "OLDPWD");
-		if (update_oldpwd(mini->env))
+		if (update_oldpwd(mini->env, mini))
 			return (1);
 
 	}
--- SRC/built-ins/export.c
+++ SRC/built-ins/export.c
@@ -6,13 +6,35 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 17:46:12 by jsebasti          #+#    #+#             */
-/*   Updated: 2023/05/17 23:43:26 by jsebasti         ###   ########.fr       */
+/*   Updated: 2023/05/18 01:59:28 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-exec_export(t_mini *mini)
+int	exec_export(t_mini *mini, char *args)
 {
-	
+	char	*tmp;
+	char	*tmp2;
+
+	tmp = ft_substr(args, "=");
+	if (!tmp)
+		return (1);
+	tmp2 = ft_strtrim(tmp);
+	if (!tmp2)
+		return (1);
+	if (search_env(mini->env, tmp2, 1))
+	{
+		mini->env = mini->env->next;
+		mini->env = malloc(sizeof(t_env));
+		if (!mini->env)
+			return (1);
+	}
+	mini->env->data = ft_strjoin(tmp2, tmp);
+	if (!mini->env->data)
+		return (1);
+	search_env(mini->env, tmp2, 2);
+	free(tmp);
+	free(tmp2);
+	return (0);	
 }
